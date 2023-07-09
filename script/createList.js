4e8f159e796f60964a5f37ed5b2c5379d63165f0
abhisheksharma <abhisheksharma131098@gmail.com>
2023-07-09 09:36:46 +0530

    changes made to script file

--- script/createList.js
+++ script/createList.js
@@ -20,6 +20,7 @@ const data = [
     id: 1,
     name: "Leanne Graham",
     qualification: "B.Tech",
+    rank: "rank",
     skills: skill1,
     yoe: 1,
     location: "India",
@@ -29,6 +30,7 @@ const data = [
     id: 2,
     name: "Ervin Howell",
     qualification: "B.Tech",
+    rank: "rank",
     skills: skill2,
     yoe: 1,
     location: "India",
@@ -38,6 +40,7 @@ const data = [
     id: 3,
     name: "Clementine Bauch",
     qualification: "B.C.A",
+    rank: "rank",
     skills: skill3,
     yoe: 2,
     location: "India",
@@ -47,6 +50,7 @@ const data = [
     id: 4,
     name: "Patricia Lebsack",
     qualification: "B.Tech",
+    rank: "rank",
     skills: skill4,
     yoe: 5,
     location: "India",
@@ -56,6 +60,7 @@ const data = [
     id: 5,
     name: "Chelsey Dietrich",
     qualification: "B.C.A",
+    rank: "rank",
     skills: skill5,
     yoe: 1,
     location: "India",
@@ -65,6 +70,7 @@ const data = [
     id: 6,
     name: "Dennis Schulist",
     qualification: "B.Tech",
+    rank: "rank",
     skills: skill6,
     yoe: 0,
     location: "India",
@@ -74,6 +80,7 @@ const data = [
     id: 7,
     name: "Kurtis Weissnat",
     qualification: "B.Tech",
+    rank: "rank",
     skills: skill1,
     yoe: 2,
     location: "India",
@@ -83,6 +90,7 @@ const data = [
     id: 8,
     name: "Nicholas Runolfsdottir V",
     qualification: "B.C.A",
+    rank: "rank",
     skills: skill2,
     yoe: 3,
     location: "India",
@@ -92,6 +100,7 @@ const data = [
     id: 9,
     name: "Glenna Reichert",
     qualification: "B.Tech",
+    rank: "rank",
     skills: skill3,
     yoe: 1,
     location: "India",
@@ -101,6 +110,7 @@ const data = [
     id: 10,
     name: "Clementina DuBuque",
     qualification: "B.C.A",
+    rank: "rank",
     skills: skill6,
     yoe: 0,
     location: "India",
@@ -127,6 +137,56 @@ function createCandidateListItem(data, doType) {
   candidateName.classList.add("candidate-name");
   candidateName.textContent = data.name;
 
+  const select = document.createElement("select");
+  select.addEventListener("click", () =>
+    select?.value != "rank" ? addPropToSavedData(data, select?.value) : null
+  );
+
+  if (doType == "remove") {
+    select.id = "dropdown";
+    select.name = "rank";
+    select.classList.add("form-control");
+    select.title = "rank";
+
+    const option1 = document.createElement("option");
+    option1.disabled = true;
+    option1.value = "rank";
+    data?.rank === "rank" ? option1.selected = true : null;
+    option1.textContent = "Rank";
+
+    const option2 = document.createElement("option");
+    option2.value = "idealmatch";
+    data?.rank === "idealmatch" ? option2.selected = true : null;
+    option2.textContent = "Ideal match";
+
+    const option3 = document.createElement("option");
+    option3.value = "excellent";
+    data?.rank === "excellent" ? option3.selected = true : null;
+    option3.textContent = "Excellent";
+
+    const option4 = document.createElement("option");
+    option4.value = "goodfit";
+    data?.rank === "goodfit" ? option4.selected = true : null;
+    option4.textContent = "Good fit";
+
+    const option5 = document.createElement("option");
+    option5.value = "consider";
+    data?.rank === "consider" ? option5.selected = true : null;
+    option5.textContent = "Consider";
+
+    const option6 = document.createElement("option");
+    option6.value = "other";
+    data?.rank === "other" ? option6.selected = true : null;
+    option6.textContent = "Other";
+
+    select.appendChild(option1);
+    select.appendChild(option2);
+    select.appendChild(option3);
+    select.appendChild(option4);
+    select.appendChild(option5);
+    select.appendChild(option6);
+  }
+
   const saveButton = document.createElement("button");
   saveButton.textContent = doType == "save" ? "Save" : "Remove";
 
@@ -139,6 +199,7 @@ function createCandidateListItem(data, doType) {
 
   row1.appendChild(image);
   row1.appendChild(candidateName);
+  doType == "remove" ? row1.appendChild(select) : null;
   row1.appendChild(saveButton);
   row1.appendChild(evaluateButton);
 
@@ -172,7 +233,7 @@ function createCandidateListItem(data, doType) {
 }
 
 searchList.addEventListener("click", () => {
-    searchList.style.background = "rgba(108, 99, 255, 0.5)";
+  searchList.style.background = "rgba(108, 99, 255, 0.5)";
   savedList.style.background = "rgba(108, 99, 255, 1)";
   generateSearchList();
   searchValue = "searchList";
@@ -205,18 +266,16 @@ function storeData(data) {
 
 //regenrate list for saved items
 savedList.addEventListener("click", () => {
-    savedList.style.background = "rgba(108, 99, 255, 0.5)";
-    searchList.style.background = "rgba(108, 99, 255, 1)";
+  savedList.style.background = "rgba(108, 99, 255, 0.5)";
+  searchList.style.background = "rgba(108, 99, 255, 1)";
   generateSavedList();
   searchValue = "savedList";
 });
 
 function generateSavedList() {
-    const retrievedData = getLocalStorageData();
-
+  const retrievedData = getLocalStorageData();
   const targetContainer = document.getElementById("list-generate");
   targetContainer.innerHTML = "";
-
   if (retrievedData?.length > 0) {
     retrievedData?.forEach((element) => {
       createCandidateListItem(element, "remove");
@@ -228,13 +287,35 @@ function generateSavedList() {
 
 //function to remove data
 function removeData(data) {
-    const retrievedData = getLocalStorageData();
+  const retrievedData = getLocalStorageData();
   const newData = retrievedData?.filter((el) => el?.id != data.id);
   // Convert the data to a JSON string
   const jsonData = JSON.stringify(newData);
   // Store the data in the local storage under a specific key
   localStorage.setItem("data", jsonData);
   generateSavedList();
+}
+
+//function to add or new property
+function addPropToSavedData(data, value) {
+  const retrievedData = getLocalStorageData();
+  const newData = retrievedData.map((el) => {
+    if (el?.id == data.id) {
+      el.rank = value;
+      return el;
+    }
+    return el;
+  });
+  if (value != "rank") {
+    // Convert the data to a JSON string
+    const jsonData = JSON.stringify(newData);
+    // Store the data in the local storage under a specific key
+    localStorage.setItem("data", jsonData);
+    //call
+    // const targetContainer = document.getElementById("list-generate");
+    // targetContainer.innerHTML = "";
+    // generateSavedList();
+  }
 }
 
 //filter list
@@ -245,14 +326,14 @@ function generateFilterList() {
   if (searchValue == "searchList") {
     const newFilterData = data.filter((el) => el?.role == jobRole);
     const targetContainer = document.getElementById("list-generate");
-  targetContainer.innerHTML = "";
-  if (newFilterData?.length > 0) {
-    newFilterData?.forEach((element) => {
-      createCandidateListItem(element, "save");
-    });
-  } else {
-    targetContainer.innerHTML = "Not any match found.";
-  }
+    targetContainer.innerHTML = "";
+    if (newFilterData?.length > 0) {
+      newFilterData?.forEach((element) => {
+        createCandidateListItem(element, "save");
+      });
+    } else {
+      targetContainer.innerHTML = "Not any match found.";
+    }
   } else if (searchValue == "savedList") {
     const retrievedData = getLocalStorageData();
     const newFilterData = retrievedData.filter((el) => el?.role == jobRole);
@@ -269,13 +350,13 @@ function generateFilterList() {
 }
 
 //function to get data from local storage
-function getLocalStorageData(){
-    // Retrieve the data from local storage
-    const storedData = localStorage.getItem("data");
-    // Convert the JSON string back to an object
-    const retrievedData = JSON.parse(storedData);
-    // Use the retrieved data
-    return retrievedData;
+function getLocalStorageData() {
+  // Retrieve the data from local storage
+  const storedData = localStorage.getItem("data");
+  // Convert the JSON string back to an object
+  const retrievedData = JSON.parse(storedData);
+  // Use the retrieved data
+  return retrievedData;
 }
 
 // Clear the entire local storage
